9070db97d7c05454b47fc22eafef95094c6f2d5e
dedyalfaris <dedyalfaris.da41@gmail.com>
2021-12-02 00:13:40 +0700

    Tugas 9 C++
    Berikut Saya Lampirkan Tugas 9 C++ Kepada Niomic

---
+++ tugas9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int x,z,y;
+    cout<<"Inputkan Nilai x  ";
+    cin>>x;
+    for(z=0;z<x;z++){
+        y=z*x;
+        cout<<y<<endl;
+       
+        
+
+    }
+    
+    }
